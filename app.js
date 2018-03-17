a0ff3c7a315e7d8844afd56b89920513a0a6df2e
Erick Smith <smith.s.erick@gmail.com>
2018-03-16 21:05:58 -0700

    added cookie session

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session')
+const passport = require('passport')
 require('./models/User');
 require('./services/passport');
 const keys = require('./config/keys');
@@ -7,6 +9,15 @@ const app = express();
 require('./routes/authRoutes')(app);
 
 mongoose.connect(keys.mongoURI);
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=>{
