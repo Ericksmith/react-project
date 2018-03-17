9a078fad684ab68a5f44fa0bc967abe219dc6db4
Erick Smith <smith.s.erick@gmail.com>
2018-03-16 20:59:24 -0700

    added passport

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('./models/User');
 require('./services/passport');
+const keys = require('./config/keys');
 const app = express();
-require('./routes/authRoutes');
 require('./routes/authRoutes')(app);
 
+mongoose.connect(keys.mongoURI);
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=>{
